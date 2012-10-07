8f05c1bc970bfe7f413e36bd5d907333b57f0af2
thekraven <savethestyle1@gmail.com>
2012-10-07 00:40:32 -0500

    text scaling

--- Source/WebCore/css/CSSStyleSelector.cpp
+++ Source/WebCore/css/CSSStyleSelector.cpp
@@ -6936,8 +6936,9 @@ float CSSStyleSelector::getComputedSizeFromSpecifiedSize(Document* document, Ren
     float zoomFactor = 1.0f;
     if (!useSVGZoomRules) {
         zoomFactor = style->effectiveZoom();
-        if (Frame* frame = document->frame())
-            zoomFactor *= frame->textZoomFactor();
+        if (!isAbsoluteSize) 
+            if (Frame* frame = document->frame()) 
+                zoomFactor *= frame->textZoomFactor(); 
     }
 
     // We support two types of minimum font size.  The first is a hard override that applies to
